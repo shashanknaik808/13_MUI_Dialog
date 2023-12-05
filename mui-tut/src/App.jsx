823881442c2db5607dee39cc4c2aa20da76c8f59
Shashank Naik <shashanknaik808@gmail.com>
2023-12-05 15:08:51 +0530

    Added Dialog Component with State Management

--- mui-tut/src/App.jsx
+++ mui-tut/src/App.jsx
@@ -4,7 +4,23 @@ import Test from './Test';
 function App() {
   return (
     <div>
-      <Test />
+      <Button onClick={() => setOpen(true)}>Open Dialog</Button>
+      <Dialog hideBackdrop open={open} onClose={() => setOpen(false)}>
+        <DialogTitle>
+          This is my Dialog
+        </DialogTitle>
+
+        <DialogContent>
+          <DialogContentText>
+            A dialog is a popup window which can ask user something like an information or confirmation.
+          </DialogContentText>
+        </DialogContent>
+
+        <DialogActions>
+          <Button onClick={() => setOpen(false)}>Cancel</Button>
+          <Button onClick={() => setOpen(false)}>Agree</Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 }
