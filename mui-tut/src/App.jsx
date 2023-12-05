b1a8b885c21d2b112819159ee00f5b1b4a777a18
Shashank Naik <shashanknaik808@gmail.com>
2023-12-05 15:12:14 +0530

    Added a Button to open a Dialog with title, content, and action buttons

--- mui-tut/src/App.jsx
+++ mui-tut/src/App.jsx
@@ -4,23 +4,7 @@ import Test from './Test';
 function App() {
   return (
     <div>
-      <Button onClick={() => setOpen(true)}>Open Dialog</Button>
-      <Dialog hideBackdrop open={open} onClose={() => setOpen(false)}>
-        <DialogTitle>
-          This is my Dialog
-        </DialogTitle>
-
-        <DialogContent>
-          <DialogContentText>
-            A dialog is a popup window which can ask user something like an information or confirmation.
-          </DialogContentText>
-        </DialogContent>
-
-        <DialogActions>
-          <Button onClick={() => setOpen(false)}>Cancel</Button>
-          <Button onClick={() => setOpen(false)}>Agree</Button>
-        </DialogActions>
-      </Dialog>
+      <Test />
     </div>
   );
 }
--- mui-tut/src/Test.jsx
+++ mui-tut/src/Test.jsx
@@ -1,8 +1,38 @@
 import React from 'react';
+import {
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle
+} from '@mui/material';
+import { useState } from 'react';
 
 function Test() {
+
+    const [open, setOpen] = useState(false);
+
     return (
-        <div>Test</div>
+        <div>
+            <Button onClick={() => setOpen(true)}>Open Dialog</Button>
+            <Dialog hideBackdrop open={open} onClose={() => setOpen(false)}>
+                <DialogTitle>
+                    This is my Dialog
+                </DialogTitle>
+
+                <DialogContent>
+                    <DialogContentText>
+                        A dialog is a popup window which can ask user something like an information or confirmation.
+                    </DialogContentText>
+                </DialogContent>
+
+                <DialogActions>
+                    <Button onClick={() => setOpen(false)}>Cancel</Button>
+                    <Button onClick={() => setOpen(false)}>Agree</Button>
+                </DialogActions>
+            </Dialog>
+        </div>
     )
 }
 
